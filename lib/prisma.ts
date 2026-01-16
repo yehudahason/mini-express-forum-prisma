0149812213a846ee2c7cae6ea57bf9598da580d1
Yehuda <yehuda@24.04>
2026-01-16 02:38:35 +0200

    setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
-// import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
