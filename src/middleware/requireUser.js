3cd1531649cae8055ec7eb668250340f7367d188
Yehuda <yehuda@24.04>
2026-01-18 18:19:45 +0200

    error message

--- src/middleware/requireUser.js
+++ src/middleware/requireUser.js
@@ -4,8 +4,10 @@ export async function requireUser(req, res, next) {
   const token = req.cookies.sb_access_token;
   if (!token) return res.render("login", { title: "Login", error: "כניסה לרשומים בלבד", success: null });
   const { data, error } = await supabase.auth.getUser(token);
-  if (error) return res.render("login", { title: "Login", error: `${error.message}`, success: null });
-
+  if (error){
+    console.log("Error fetching user:", error.message);
+    return res.render("login", { title: "Login", error: "Please Login Again", success: null });
+  }
   req.user = data.user;
   next();
 }
